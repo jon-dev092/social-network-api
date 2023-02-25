16d2234d305c5f5a6437267cae538360c36c643c
jon-dev092 <jp106833@yahoo.com>
2023-02-25 16:37:22 -0500

    added file structure, config and model functionality, set up server

---
+++ README.md
@@ -0,0 +1,20 @@
+# Social-Network-API Backend Challenge
+
+## Description:
+<!-- in the works  -->
+
+## Install:
+1. Run "npm i" to install packages
+2. Run "npm run start" to start the server on your terminal.
+3. Use insomnia to get/post/put/delete data 
+4. Create cool usernames and share your thoughts!
+ 
+
+## Usage:
+<!-- in the works -->
+
+
+
+## Links:
+[Github Repo](https://github.com/jon-dev092/social-network-api)
+
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    thought: [{
+      type: Schema.Types.ObjectId,
+      ref: 'thought'
+    }]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
