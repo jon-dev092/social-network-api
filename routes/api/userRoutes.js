c961f892d420b52f23e32c0c284d68cccdd22f48
jon-dev092 <jp106833@yahoo.com>
2023-02-25 18:52:12 -0500

    added user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,26 @@
 const router = require('express').Router();
+
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require('../../controllers/userController.js');
+
+// /api/users
+router.route('/').get(getUsers);
+
+// /api/users/:_id
+router.route('/:_id').get(getSingleUser);
+
+// /api/users/createUser
+router.route('/createUser').post(createUser);
+
+// /api/users/updateUser/:_id
+router.route('/updateUser/:_id').put(updateUser)
+
+// /api/users/deleteUser/:_id
+router.route('/deleteUser/:_id').delete(deleteUser)
+
+module.exports = router;
