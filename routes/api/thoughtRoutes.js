bfb343f833575e951adaeecac6ebf68101c59ca3
jon-dev092 <jp106833@yahoo.com>
2023-02-25 19:12:48 -0500

    added thought routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,26 @@
 const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+  } = require('../../controllers/thoughtController');
+  
+  // /api/thoughts
+  router.route('/').get(getThoughts);
+  
+  // /api/thoughts/:_id
+  router.route('/:_id').get(getSingleThought);
+  
+  // /api/thoughts/createthought
+  router.route('/createThought').post(createThought);
+  
+  // /api/thoughts/updateThought/:thoughtId
+  router.route('/updateThought/:_id').put(updateThought);
+  
+  // /api/thoughts/deletethought/:_id
+  router.route('/deleteThought/:_id').delete(deleteThought);
+  
+  module.exports = router;
